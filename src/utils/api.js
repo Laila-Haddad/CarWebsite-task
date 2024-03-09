805c7256bc31a367044c1bb422561d1648af9963
Laila-Haddad <108059233+Laila-Haddad@users.noreply.github.com>
2024-03-09 22:34:43 +0300

    Initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,41 +1,74 @@
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+// // const url = "./data.json";
 
-export async function getCars() {
-  const res = await fetch(`${url}/cars`);
-  const data = await res.json();
-  return data;
-}
 
-export async function getBrands() {
-  const res = await fetch(`${url}/brands`);
-  const data = await res.json();
-  return data;
-}
+// export async function getCars() {
+//   const res = await fetch(`${url}/cars`);
+//   const data = res.json();
 
-export async function getCarDetails(id) {
-  const res = await fetch(`${url}/cars/${id}`);
-  const data = await res.json();
-  return data;
-}
+//   return data;
+// }
 
-export async function getCart() {
-  const res = await fetch(`${url}/cart`);
-  const data = await res.json();
-  return data;
-}
+// export async function getBrands() {
+//   const res = await fetch(`${url}/brands`);
+//   const data = await res.json();
+//   return data;
+// }
+
+// export async function getCarDetails(id) {
+//   const res = await fetch(`${url}/cars/${id}`);
+//   const data = await res.json();
+//   return data;
+// }
+
+// export async function getCart() {
+//   const res = await fetch(`${url}/cart`);
+//   const data = await res.json();
+//   return data;
+// }
 
 export async function deleteItem(id) {
-  const res = await fetch(`${url}/cart/${id}`, {
-    method: "DELETE",
-  });
+  // const res = await fetch(`${url}/cart/${id}`, {
+  //   method: "DELETE",
+  // });
 }
 
 export async function addToCart(data) {
-  const res = await fetch(`${url}/cart`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-type": "application/json",
-    },
-  });
+  // const res = await fetch(`${url}/cart`, {
+  //   method: "POST",
+  //   body: JSON.stringify(data),
+  //   headers: {
+  //     "Content-type": "application/json",
+  //   },
+  // });
 }
+
+
+const url = "/data.json";
+
+export async function getCars() {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.cars;
+}
+
+export async function getBrands() {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.brands;
+}
+
+export async function getCarDetails(id) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const car = data.cars.find(car => car.id.toString() === id); 
+  return car;
+}
+
+export async function getCart() {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.cart;
+}
+
