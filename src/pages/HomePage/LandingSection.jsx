b6f028c5effcdc1bd7d46a0b98b4e7072cc7ffb8
Laila-Haddad <108059233+Laila-Haddad@users.noreply.github.com>
2024-03-14 10:28:34 +0300

    landing section image fix

--- src/pages/HomePage/LandingSection.jsx
+++ src/pages/HomePage/LandingSection.jsx
@@ -13,14 +13,12 @@ function LandingSection() {
   return (
     <section className="landing">
           <div>
-          <img className='landing-img-small' src={carPic2} alt="Car" />
             <h6>Arriving Summer 2019</h6>
             <h1>MAKE EVERY DAY LEGENDARY.</h1>
             <p className="landing-description">
               The culmination of comfort, luxury, and powerful living is
               embodied in the First-Ever BMUX7 - the biggest BMU ever built.
             </p>
-          {/* <img className='landing-car-small' src={carPic3} alt="Car"  /> */}
 
             <a href="#products"><button className="filled-btn" >EXPLORE</button></a>
           </div>
